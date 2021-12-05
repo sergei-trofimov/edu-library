6c537cfeccc3612cc4e71a48ddaafc06ddc5e8e8
sergei-trofimov <feast.st@gmail.com>
2021-12-05 20:29:56 +0200

    Added translating and firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyA5w3KM3LT_UhseRfPeY5awu5o8q_q8-I0",
+    authDomain: "edulibrary-d7f8e.firebaseapp.com",
+    databaseURL: "https://edulibrary-d7f8e-default-rtdb.firebaseio.com",
+    projectId: "edulibrary-d7f8e",
+    storageBucket: "edulibrary-d7f8e.appspot.com",
+    messagingSenderId: "416291166217",
+    appId: "1:416291166217:web:81494ed5a103c92500c0d8",
+    measurementId: "G-JZNVTHM8CZ"
+  },
 };
